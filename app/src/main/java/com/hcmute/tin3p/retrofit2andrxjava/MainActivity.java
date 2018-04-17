9c7552493998c5c24140f5254d6177f634ebe636
Tin Truong <thanhtin96@gmail.com>
2018-04-17 14:08:45 +0700

    first commit

---
+++ app/src/main/java/com/hcmute/tin3p/retrofit2andrxjava/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hcmute.tin3p.retrofit2andrxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
